8054e1cd67d3384762d7bf8e667f48cca81c97b7
mckavin <154789241+mckavin@users.noreply.github.com>
2024-03-30 16:53:20 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.97.1"
+    }
+  }
+}
+
+
+provider "azurerm" {
+  # Configuration options
+  subscription_id = "e60e30b7-790b-456f-b7d6-baa205e33018"
+  tenant_id = "a9e01f08-5531-4b9d-971c-732eab18ff4c"
+  client_id = "4536dcdc-1fac-4e20-9e7b-42d19fa98963"
+  client_secret = "J5p8Q~31065w2gyRWxuHKPM7jnt2LW5lqGkxactx"
+  features {
+    
+  }
+
+}
+
+resource "azurerm_resource_group" "test_rg1" {
+  name     = "test_rg1"
+  location = "EastUS2"
+  tags ={
+    "env"="dev"
+
+  }
+}
